33b09f9d7c55b2b8518fc766898aabb6cf510621
Jorge Pereira <jff.pereira@campus.fct.unl.pt>
2019-11-21 09:13:43 +0000

    Trabalho

--- TP2.py
+++ TP2.py
@@ -1,35 +1,60 @@
 # -*- coding: utf-8 -*-
 
 from extraction import extract_feature
-from tp2_aux import images_as_matrix
-import cluster
+from tp2_aux import images_as_matrix,report_clusters
+from cluster import kmean_cluster,dbsan_cluster
 import numpy as np
 
 FEATURES = 18
 SIZE = 564
 N_IMAGES = 563
-EXTRACT = True
+EXTRACT = False
 
-def extract_features_or_read_file(data):
-    featuresPerImage = []
-    
+
+def extract_features_or_read_file2(data):    
     if EXTRACT:
-        for i in range(0,N_IMAGES):
-            ft = extract_feature(data[i].reshape(50,50))
-            np.savez(f"ft/{i}",ft)
-            featuresPerImage.append(ft)
+            ft = extract_feature(data)
+            np.savez(f"ft/extracted", ft)
+            return ft
     else:
-        for i in range(0,N_IMAGES):
-            file = np.load(f"ft/{i}.npz")
-            featuresPerImage.append(file['arr_0'])
+            return np.load("ft/extracted.npz")['arr_0']
+
+
+def check_cluster(labels):
+    l = np.loadtxt("labels.txt",skiprows=1,delimiter=",")
+    und = 0;
+    right = 0;
+    wrong = 0;
+    for i in range(N_IMAGES - 1):
+        if l[i][1] == 0:
+            und = und + 1
+        else:
+            if l[i][1] == labels[i] + 1:
+                right = right + 1
+            else:
+                wrong = wrong + 1
+    print(f"und:{und}")
+    print(f"right:{right}")
+    print(f"wrong:{wrong}")
     
-    return featuresPerImage
 
 def main():
     data = images_as_matrix()
-    featuresPerImage = extract_features_or_read_file(data)
-
+    featuresPerImage = extract_features_or_read_file(data)[:,[15,11]]
+    check_cluster( kmean_cluster(featuresPerImage))
+    check_cluster( dbsan_cluster(featuresPerImage))
+    report_clusters(np.linspace(0, N_IMAGES, num=N_IMAGES), kmean_cluster(featuresPerImage), "kmean.html")
+    report_clusters(np.linspace(0, N_IMAGES, num=N_IMAGES), dbsan_cluster(featuresPerImage), "dbsan.html")
 
     return featuresPerImage
 
-test = main()
+t = main()
+
+
+#from sklearn.feature_selection import f_classif
+
+
+#l = np.loadtxt("labels.txt",skiprows=1,delimiter=",")
+#X = t[l[:,1] > 0]
+#y = l[l[:,1] > 0][:,1]
+#f,prob = f_classif(X,y)
--- cluster.py
+++ cluster.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
+from sklearn.cluster import KMeans,DBSCAN
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
 
 def kmean_cluster(data):
-    return cluster.KMeans(n_clusters=3).fit_predict(data)
+    return KMeans(n_clusters=3,max_iter=300).fit_predict(data)
+
+def dbsan_cluster(data):
+    e = tunning_eps(data)
+    return DBSCAN(eps=e, min_samples=2).fit_predict(data)
+
+
+def tunning_eps(data):
+    zeros = [0] * 563
+    neigh = KNeighborsClassifier(n_neighbors=5).fit(X=data,y=zeros)
+    distances, indices = neigh.kneighbors(data)
+    
+    sort = np.sort(distances, axis=0)
+    print(distances)
+    e = 0
+    for i in range(0,562):
+        e = e + sort[i][4]
+    
+    e = e / 562
+    print(f"distance:{e}")
+    return e
+    
